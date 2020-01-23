6fbce0289fdcbfe0ded7e8c0f9d47fe6164ad71e
Mr. G Yo! <codemg@gmail.com>
2020-01-23 12:33:42 -0500

    Added palindrome test app

--- Stacks_And_Queues/Program.cs
+++ Stacks_And_Queues/Program.cs
@@ -7,9 +7,38 @@ namespace Stacks_And_Queues
         static void Main(string[] args)
         {
             CSTack aList = new CSTack();
+            string ch;
+            string word = "sees";
+            bool isPalindrome = true;
 
-            Console.WriteLine("Hello World!");
-            // tst upload
+            for (int x = 0; x < word.Length; x++)
+            {
+                aList.push(word.Substring(x,1));
+
+            }
+
+            int pos = 0;
+
+            while (aList.count > 0)
+            {
+                ch = aList.pop().ToString();
+                if (ch != word.Substring(pos,1))
+                {
+                    isPalindrome = false;
+                    break;
+                }
+                pos++;
+            }
+            
+            if (isPalindrome)
+            {
+                Console.WriteLine($"{word} is a palindrome.");
+            }
+            else
+            {
+                    Console.WriteLine($"{word} is not a palindrome.");
+
+            }
         }
     }
 }
