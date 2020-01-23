fccd6364b53c4fbdc28c9225b59988106969bb0e
Mr. G Yo! <codemg@gmail.com>
2020-01-23 12:38:03 -0500

    Git Practice push :)

--- Program.cs
+++ Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
-using System.Collections;
 
-public class Program
+namespace Stacks_And_Queues
 {
-	static void Main(string[] args)
-	{
-		CStack alist = new CStack();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSTack aList = new CSTack();
+            string ch;
+            string word = "sees";
+            bool isPalindrome = true;
 
-		
-	}
+            for (int x = 0; x < word.Length; x++)
+            {
+                aList.push(word.Substring(x, 1));
+
+            }
+
+            int pos = 0;
+
+            while (aList.count > 0)
+            {
+                ch = aList.pop().ToString();
+                if (ch != word.Substring(pos, 1))
+                {
+                    isPalindrome = false;
+                    break;
+                }
+                pos++;
+            }
+
+            if (isPalindrome)
+            {
+                Console.WriteLine($"{word} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{word} is not a palindrome.");
+
+            }
+        }
+    }
 }
